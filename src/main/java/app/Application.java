02afc0d95b2a9d3d4e250b64d46fff7ed3a66f10
belovmv.24 <maksimbelov2006@gmail.com>
2022-12-22 11:34:29 +0300

    init

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,117 @@
+package app;
+
+import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Canvas;
+import io.github.humbleui.skija.Paint;
+import io.github.humbleui.skija.RRect;
+import io.github.humbleui.skija.Surface;
+import misc.CoordinateSystem2i;
+import misc.Misc;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import static app.Colors.APP_BACKGROUND_COLOR;
+
+public class Application implements Consumer<Event> {
+    /**
+     * радиус скругления элементов
+     */
+    public static final int C_RAD_IN_PX = 4;
+    /**
+     *
+     */
+    // окно приложения
+    private final Window window;
+
+    // конструктор приложения
+    /**
+     * Конструктор окна приложения
+     */
+    public Application() {
+        // создаём окно
+        window = App.makeWindow();
+        // задаём обработчиком событий текущий объект
+        window.setEventListener(this);
+        // задаём заголовок
+        window.setTitle("Java 2D");
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+        // задаём его положение
+        window.setWindowPosition(100, 100);
+        // задаём иконку
+
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
+
+        // названия слоёв, которые будем перебирать
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+
+        // перебираем слои
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        // если окну не присвоен ни один из слоёв
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
+
+        // делаем окно видимым
+        window.setVisible(true);
+    }
+
+
+
+    /**
+     * Обработчик событий
+     *
+     * @param e событие
+     */
+    @Override
+    public void accept(Event e) {
+        // если событие - это закрытие окна
+        if (e instanceof EventWindowClose) {
+            // завершаем работу приложения
+            App.terminate();
+        } else if (e instanceof EventWindowCloseRequest) {
+            window.close();
+    } else if (e instanceof EventFrameSkija ee) {
+        Surface s = ee.getSurface();
+        paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth()/3, s.getHeight()/3, s.getWidth()/3, s.getHeight()/3));
+    }
+
+    }
+    /**
+     * Рисование
+     *
+     * @param canvas   низкоуровневый инструмент рисования примитивов от Skija
+     * @param windowCS СК окна
+     */
+    public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // запоминаем изменения (пока что там просто заливка цветом)
+        canvas.save();
+        // очищаем канвас
+        canvas.clear(APP_BACKGROUND_COLOR);
+        // создаём кисть
+        Paint paint = new Paint();
+        // задаём цвет рисования
+        paint.setColor(Misc.getColor(100, 255, 255, 255));
+        // рисуем квадрат
+        canvas.drawRRect(windowCS.getRRect(4), paint);
+        // восстанавливаем состояние канваса
+        canvas.restore();
+    }
+
+}

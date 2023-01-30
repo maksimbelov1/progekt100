fa7c3167a3035fa1fe474c64e5ea967a146226e1
belovmv.24 <maksimbelov2006@gmail.com>
2023-01-30 11:26:33 +0300

    solve demo

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -1,6 +1,7 @@
 package app;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.humbleui.jwm.MouseButton;
@@ -16,6 +17,9 @@ import panels.PanelLog;
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static app.Colors.CROSSED_COLOR;
+import static app.Colors.SUBTRACTED_COLOR;
 
 /**
  * Класс задачи
@@ -41,6 +45,20 @@ public class Task {
      */
     @Getter
     private final ArrayList<Point> points;
+
+    /**
+     * Список точек в пересечении
+     */
+    @Getter
+    @JsonIgnore
+    private final ArrayList<Point> crossed;
+    /**
+     * Список точек в разности
+     */
+    @Getter
+    @JsonIgnore
+    private final ArrayList<Point> single;
+
     /**
      * Размер точки
      */
@@ -74,8 +92,33 @@ public class Task {
      * Решить задачу
      */
     public void solve() {
+        // очищаем списки
+        crossed.clear();
+        single.clear();
+
+        // перебираем пары точек
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                // сохраняем точки
+                Point a = points.get(i);
+                Point b = points.get(j);
+                // если точки совпадают по положению
+                if (a.pos.equals(b.pos) && !a.pointSet.equals(b.pointSet)) {
+                    if (!crossed.contains(a)){
+                        crossed.add(a);
+                        crossed.add(b);
+                    }
+                }
+            }
+        }
+
+        /// добавляем вс
+        for (Point point : points)
+            if (!crossed.contains(point))
+                single.add(point);
+
+        // задача решена
         solved = true;
-        PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
 
     /**
@@ -94,6 +137,7 @@ public class Task {
     public boolean isSolved() {
         return solved;
     }
+
 
 
 
@@ -163,6 +207,9 @@ public class Task {
     ) {
         this.ownCS = ownCS;
         this.points = points;
+
+        this.crossed = new ArrayList<>();
+        this.single = new ArrayList<>();
     }
     /**
      * Добавить точку
@@ -180,7 +227,6 @@ public class Task {
 
 
 
-
     /**
      * Рисование задачи
      *
@@ -188,26 +234,29 @@ public class Task {
      * @param windowCS СК окна
      */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // Сохраняем последнюю СК
+        lastWindowCS = windowCS;
+
         canvas.save();
         // создаём перо
         try (var paint = new Paint()) {
             for (Point p : points) {
-                // получаем цвет точки
-                paint.setColor(p.getColor());
+                if (!solved) {
+                    paint.setColor(p.getColor());
+                } else {
+                    if (crossed.contains(p))
+                        paint.setColor(CROSSED_COLOR);
+                    else
+                        paint.setColor(SUBTRACTED_COLOR);
+                }
                 // y-координату разворачиваем, потому что у СК окна ось y направлена вниз,
                 // а в классическом представлении - вверх
                 Vector2i windowPos = windowCS.getCoords(p.pos.x, p.pos.y, ownCS);
                 // рисуем точку
                 canvas.drawRect(Rect.makeXYWH(windowPos.x - POINT_SIZE, windowPos.y - POINT_SIZE, POINT_SIZE * 2, POINT_SIZE * 2), paint);
-
             }
         }
         canvas.restore();
-
-        // Сохраняем последнюю СК
-        lastWindowCS = windowCS;
-
-
     }
 
 }
--- src/main/java/panels/PanelControl.java
+++ src/main/java/panels/PanelControl.java
@@ -212,6 +212,21 @@ public class PanelControl extends GridPanel {
         });
 
 
+        solve.setOnClick(() -> {
+            if (!PanelRendering.task.isSolved()) {
+                PanelRendering.task.solve();
+                String s = "Задача решена\n" +
+                        "Пересечений: " + PanelRendering.task.getCrossed().size() / 2 + "\n" +
+                        "Отдельных точек: " + PanelRendering.task.getSingle().size();
+                PanelLog.success(s);
+                solve.text = "Сбросить";
+            } else {
+                cancelTask();
+            }
+            window.requestFrame();
+        });
+
+
 
 
     }
